500fe1b6d42c0d61a90b95a41c058cc54055ac9f
Gaston Mollica <mollica.gaston@gmail.com>
2018-02-21 20:17:02 +1000

    subbing new words

--- substitute.rb
+++ substitute.rb
@@ -3,3 +3,12 @@ print "Input please: "
 user_input = gets.chomp
 user_input.downcase!
 
+if user_input.include? "f"
+	user_input.gsub!(/f/, "ph")
+	puts "Here is your new input: #{user_input}"
+
+else 
+	puts "Your input is: #{user_input}"
+
+end
+
